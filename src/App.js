124aa8651c2de884281f08027f25602f1bb0de2d
TimurSejati <timur7dian84@gmail.com>
2020-09-16 20:54:12 +0700

    Css Grid & Flexbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navigation from "./components/navigation";
+import Navigation from "./components/common/navigation";
 import "./assets/scss/base.scss";
 import PageRenderer from "./page-renderer";
 import {
---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,39 @@
+import moment from "moment";
+
+export default [
+  {
+    title: "Software Engineer Salary in 2020",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech Culture"],
+    link: "#",
+    image: "money.jpg",
+  },
+  {
+    title: "GraphQL vs Rest ",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["React", "Javascript"],
+    link: "#",
+    image: "graphql.png",
+  },
+  {
+    title: "A Day in the Life of a Programmer",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Tech Culture"],
+    link: "#",
+    image: "puzzled_programmer.jpg",
+  },
+  {
+    title: "Brain Hacks for Learning to Program",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Brain Health"],
+    link: "#",
+    image: "neuron.jpg",
+  },
+  {
+    title: "React vs Vue",
+    date: moment().format("MMMM DD, YYYY"),
+    categories: ["Javascript", "Vue"],
+    link: "#",
+    image: "vue.png",
+  },
+];
